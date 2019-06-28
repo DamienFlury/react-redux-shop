9a941be7ab24113694c1753c760118df552e9b10
Damien Flury <damien.flury@gmail.com>
2019-06-29 01:05:16 +0200

    Add Shopping cart component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Login from './components/Login';
 import { useSelector } from 'react-redux';
 import NavBar from './components/NavBar';
 import Products from './components/Products';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   const authState = useSelector(state => state.authentication);
   return (
     <div>
       <NavBar />
-      {authState.isAuthenticated ? <Products /> : <Login />}  
+      {!authState.isAuthenticated ? <Login /> : (
+        <Router>
+          <Switch>
+            <Route path="/shopping-cart" component={ShoppingCart} />
+            <Route path="/" component={Products} />
+          </Switch>
+        </Router>
+      )}    
     </div>
   );
 }
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+
+const ShoppingCart = () => {
+    const cart = useSelector(state => state.cart);
+    return (
+        <div>
+            Hey
+            {cart.length}
+        </div>
+    )
+};
+
+export default ShoppingCart;
