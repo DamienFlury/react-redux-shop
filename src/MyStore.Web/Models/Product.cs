3323d7ba5419e0f36a87598c43ccd99bbf7ca834
Damien Flury <damien.flury@gmail.com>
2019-07-16 11:59:39 +0200

    Integrated react project in .net core

--- src/MyStore.Web/Models/Product.cs
+++
@@ -1,7 +0,0 @@
-namespace MyStore.Web.Models {
-    public class Product {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public decimal Cost { get; set; }
-    }
-}
