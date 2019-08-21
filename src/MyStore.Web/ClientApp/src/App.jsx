e41167ac65b829ac2a827b0c60ff895354ccc3ff
Damien Flury <damien.flury@gmail.com>
2019-08-21 10:33:11 +0200

    Add firebase

--- src/MyStore.Web/ClientApp/src/App.jsx
+++ src/MyStore.Web/ClientApp/src/App.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
 import Products from './components/Products';
 import Purchases from './components/Purchases';
+import store from './store/store';
+
+const theme = {
+  palette: {
+    primary: ['#ff3366', '#f83366', '#ee3366'],
+    secondary: ['#00aaff', '#0099dd', '#0088cc'],
+  },
+  shadows: ['0 1px 3px #999999', '0 1px 4px #999999', '0 1px 5px #999999'],
+  borderRadius: '4px',
+  fontFamily: 'Open Sans, sans-serif',
+};
 
 function App() {
   return (
-    <div>
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/products" component={Products} />
-          <Route path="/shopping-cart" component={ShoppingCart} />
-          <Route path="/purchases" component={Purchases} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/shopping-cart" component={ShoppingCart} />
+            <Route path="/purchases" component={Purchases} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
